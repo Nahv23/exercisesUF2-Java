51ae5b1710545de604aad25332368e5e617fdf30
Nahv23 <nereaparicio@gmail.com>
2020-11-04 20:49:19 +0100

    Delete some code unuseful

--- exercise1/src/exercise1/Start.java
+++ exercise1/src/exercise1/Start.java
@@ -6,22 +6,13 @@ import java.util.Scanner;
  	1.- Crear una función llamada factorial a la cual se le pasa un único parámetro llamado numero de
 	tipo entero. La función devuelve el factorial de ese número. En caso que el parámetro sea 0 o
 	inferior devuelve un -1.
- */
+*/
 
 public class Start {
 
 	static Scanner reader = new Scanner(System.in);
 
-	public static boolean isBiggerThan0(int number) {
-		if (number <= 0)
-			return false;
-		else
-			return true;
-	}
-
 	public static double factorial(int number) {
-		
-		
 		if (number > 0) {
 			double resultFactorial = 1D;
 			for (int i = number; i > 0; i--) {
